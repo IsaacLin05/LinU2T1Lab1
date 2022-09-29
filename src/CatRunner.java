7b445a5200a1f38fd0ced1d4e86a03cd3f861973
NajamSR <najamsaqibr@nycstudents.net>
2022-09-29 09:31:11 -0400

    Update

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class CatRunner {
+public class CatRunner {
+    public static void main(String[] args)
+    {
+    Cat cat1= new Cat("Ine",12,20.6);
+    cat1.introduce();
+    cat1.printCatInfo();
+
+    Cat cat2= new Cat("Tom",4,16.2);
+    cat2.introduce();
+    cat2.printCatInfo();
+
+
+    }
 }
